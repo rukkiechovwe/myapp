26fcdea9d22d2a1482de323e85113d8277fc10ad
rukkiechovwe <rukkiechovwe@gmail.com>
2020-08-30 16:09:14 +0100

    Updates

---
+++ precache-manifest.5d28064e53289b9294966afb1b25a0f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e25c378c5d3cf95a4c43cdfbdeb5bc50",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "1b6bdbf738ad80a51025",
+    "url": "/myapp/static/css/main.6201ff27.chunk.css"
+  },
+  {
+    "revision": "712bcd67f4aba858fac9",
+    "url": "/myapp/static/js/2.8894f2a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myapp/static/js/2.8894f2a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b6bdbf738ad80a51025",
+    "url": "/myapp/static/js/main.c67618dd.chunk.js"
+  },
+  {
+    "revision": "2413d325ab07ed7ad194",
+    "url": "/myapp/static/js/runtime-main.0abbbebf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
